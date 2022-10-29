8897b853bd848bd3148f77e0b1cb27a409ff48d4
huleilei9 <huleilei9@jd.com>
2022-10-29 16:25:07 +0800

    init

---
+++ super-mall-api/src/main/java/com/hill/mall/WebApplication.java
@@ -0,0 +1,20 @@
+package com.hill.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * WebApplication
+ *
+ * @Author huleilei9
+ * @Date 2022/10/29
+ **/
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+
+
+    }
+}
